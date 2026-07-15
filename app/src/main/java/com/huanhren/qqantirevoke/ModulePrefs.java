b189205f744028506fd363662dbc0fc02a749f9a
HuanHren <102816507+HuanHren@users.noreply.github.com>
2026-07-16 03:50:05 +0800

    feat: add module preferences

---
+++ app/src/main/java/com/huanhren/qqantirevoke/ModulePrefs.java
@@ -0,0 +1,16 @@
+package com.huanhren.qqantirevoke;
+
+public final class ModulePrefs {
+    public static final String MODULE_PACKAGE = "com.huanhren.qqantirevoke";
+    public static final String PREF_FILE = "settings";
+
+    public static final String KEY_ENABLED = "enabled";
+    public static final String KEY_AGGRESSIVE = "aggressive_compatibility";
+    public static final String KEY_DIAGNOSTICS = "diagnostics";
+
+    public static final boolean DEFAULT_ENABLED = true;
+    public static final boolean DEFAULT_AGGRESSIVE = true;
+    public static final boolean DEFAULT_DIAGNOSTICS = true;
+
+    private ModulePrefs() {}
+}
